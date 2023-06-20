1beea639c296070e8b1aa3fa3fa84464f2867f32
Thanathorn15 <thanathorn_ko@kkumail.com>
2023-06-20 16:39:08 +0700

    feat: modelReswitchProfile modelReswitchReply modelLike

---
+++ src/models/like.js
@@ -0,0 +1,36 @@
+module.exports = (sequelize, DataTypes) => {
+    const Like = sequelize.define("Like", {}, { underscored: true });
+  
+    Like.associate = (models) => {
+        Like.hasMany(models.Notification, {
+            foreignKey: {
+              name: "likeId",
+              allowNull: false,
+            },
+            onDelete: "RESTRICT",
+          });
+      Like.belongsTo(models.Post, {
+        foreignKey: {
+          name: "postId",
+          allowNull: false,
+        },
+        onDelete: "RESTRICT",
+      });
+      Like.belongsTo(models.User, {
+        foreignKey: {
+          name: "userId",
+          allowNull: false,
+        },
+        onDelete: "RESTRICT",
+      });
+      Like.belongsTo(models.Reply, {
+        foreignKey: {
+          name: "replyId",
+          allowNull: false,
+        },
+        onDelete: "RESTRICT",
+      });
+  };
+  return Like;
+  };
+  
