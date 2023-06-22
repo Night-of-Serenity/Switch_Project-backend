c9a22fbd47b31f2405d526d8662ecbd8a38300a1
qMaxFcq <maxzazafc123@gmail.com>
2023-06-22 12:27:22 +0700

    fix name authenticate

--- src/app.js
+++ src/app.js
@@ -1,42 +1,41 @@
-require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
-const morgan = require("morgan");
-const createServer = require("./services/socketioService");
-const helmet = require("helmet");
-const rateLimit = require("express-rate-limit");
-const authRoute = require("../src/routes/authRoute");
-const notFoundMiddleware = require("../src/middlewares/notFoundMiddleware");
-const errorMiddleware = require("../src/middlewares/errorMiddleware");
-const authenticate = require("./middlewares/authenticate");
+require("dotenv").config()
+const express = require("express")
+const cors = require("cors")
+const morgan = require("morgan")
+const createServer = require("./services/socketioService")
+const helmet = require("helmet")
+const rateLimit = require("express-rate-limit")
+const authRoute = require("../src/routes/authRoute")
+const notFoundMiddleware = require("../src/middlewares/notFoundMiddleware")
+const errorMiddleware = require("../src/middlewares/errorMiddleware")
+const authenticateMiddleware = require("./middlewares/authenticateMiddleware")
 
-const app = express();
+const app = express()
 
 if (process.env.NODE_ENV === "development") {
-  app.use(morgan("combined"));
+    app.use(morgan("combined"))
 }
 
 app.use(
-  rateLimit({
-    windowMs: 1000 * 60 * 1,
-    max: 1000,
-    message: { message: "too many requests" },
-  })
-);
+    rateLimit({
+        windowMs: 1000 * 60 * 1,
+        max: 1000,
+        message: { message: "too many requests" },
+    })
+)
 
-app.use(cors());
-app.use(helmet());
-app.use(express.json());
+app.use(cors())
+app.use(helmet())
+app.use(express.json())
 
-app.use("/auth", authRoute);
+app.use("/auth", authRoute)
 
+app.use(notFoundMiddleware)
+app.use(errorMiddleware)
 
-app.use(notFoundMiddleware);
-app.use(errorMiddleware);
+const server = createServer(app)
 
-const server = createServer(app);
-
-const port = process.env.PORT || 7000;
+const port = process.env.PORT || 7000
 server.listen(port, () => {
-  console.log("Server is running on port ", port);
-});
+    console.log("Server is running on port ", port)
+})
