3fb4ea9a747283cde027c1e94522189a2b5534cc
qMaxFcq <maxzazafc123@gmail.com>
2023-06-22 15:29:00 +0700

    fix followSeed

--- src/dbsync/allSeed.js
+++ src/dbsync/allSeed.js
@@ -1,21 +1,21 @@
-const postSeed = require("../dbsync/postSeed")
-const postToTagSeed = require("../dbsync/postToTagSeed")
-const tagSeed = require("../dbsync/tagSeed")
-const userSeed = require("../dbsync/userSeed")
-const followSeed = require("../dbsync/followSeed")
+const postSeed = require("../dbsync/postSeed");
+const postToTagSeed = require("../dbsync/postToTagSeed");
+const tagSeed = require("../dbsync/tagSeed");
+const userSeed = require("../dbsync/userSeed");
+const followSeed = require("../dbsync/followSeed");
 
 const allSeedSync = async () => {
     try {
-        await userSeed()
-        await postSeed()
-        await tagSeed()
-        await postToTagSeed()
-        await followSeed()
+        await userSeed();
+        await postSeed();
+        await tagSeed();
+        await postToTagSeed();
+        await followSeed();
     } catch (err) {
-        console.log(err)
+        console.log(err);
     }
 
-    console.log("object")
-}
+    console.log("object");
+};
 
-allSeedSync()
+allSeedSync();
