4d061a2dcfc4f5e06c1f62fc80fa18b252304da5
qMaxFcq <maxzazafc123@gmail.com>
2023-06-22 13:57:16 +0700

    mock-db-followSeed

--- src/dbsync/allSeed.js
+++ src/dbsync/allSeed.js
@@ -1,19 +1,21 @@
-const postSeed = require("../dbsync/postSeed");
-const postToTagSeed = require("../dbsync/postToTagSeed");
-const tagSeed = require("../dbsync/tagSeed");
-const userSeed = require("../dbsync/userSeed");
+const postSeed = require("../dbsync/postSeed")
+const postToTagSeed = require("../dbsync/postToTagSeed")
+const tagSeed = require("../dbsync/tagSeed")
+const userSeed = require("../dbsync/userSeed")
+const followSeed = require("../dbsync/followSeed")
 
 const allSeedSync = async () => {
-  try {
-    await userSeed();
-    await postSeed();
-    await tagSeed();
-    await postToTagSeed();
-  } catch (err) {
-    console.log(err);
-  }
+    try {
+        await userSeed()
+        await postSeed()
+        await tagSeed()
+        await postToTagSeed()
+        await followSeed()
+    } catch (err) {
+        console.log(err)
+    }
 
-  console.log("object");
-};
+    console.log("object")
+}
 
-allSeedSync();
+allSeedSync()
