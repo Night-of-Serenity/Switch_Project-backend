bdfd563ee1acda58a25b3daa5514d9d7b048fc6d
rungkuntan <rungkun.patjinda1995@gmail.com>
2023-06-25 23:50:33 +0700

    done mock data for user follow post reply tag like and reswitchprofile

--- src/dbsync/replySeed.js
+++ src/dbsync/replySeed.js
@@ -1,4 +1,4 @@
-const { Reply } = require("../models")
+const { Reply } = require("../models");
 
 const userSeed = async () => {
     const replyData = [
@@ -9,132 +9,132 @@ const userSeed = async () => {
             imageUrl: "http://dummyimage.com/127x100.png/cc0000/ffffff",
         },
         {
-            postId: 2,
+            postId: 1,
             userId: 2,
             textcontent:
                 "In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante.",
             imageUrl: "http://dummyimage.com/164x100.png/dddddd/000000",
         },
         {
-            postId: 3,
+            postId: 1,
             userId: 3,
             textcontent: "Donec posuere metus vitae ipsum. Aliquam non mauris.",
             imageUrl: "http://dummyimage.com/117x100.png/cc0000/ffffff",
         },
         {
-            postId: 4,
+            postId: 1,
             userId: 4,
             textcontent:
                 "Maecenas leo odio, condimentum id, luctus nec, molestie sed, justo.",
             imageUrl: "http://dummyimage.com/140x100.png/ff4444/ffffff",
         },
         {
-            postId: 5,
+            postId: 2,
             userId: 5,
             textcontent: "Sed ante.",
             imageUrl: "http://dummyimage.com/128x100.png/5fa2dd/ffffff",
         },
         {
-            postId: 6,
+            postId: 2,
             userId: 6,
             textcontent: "Donec posuere metus vitae ipsum. Aliquam non mauris.",
             imageUrl: "http://dummyimage.com/135x100.png/5fa2dd/ffffff",
         },
         {
-            postId: 7,
+            postId: 2,
             userId: 7,
             textcontent: "Nulla suscipit ligula in lacus.",
             imageUrl: "http://dummyimage.com/100x100.png/ff4444/ffffff",
         },
         {
-            postId: 8,
+            postId: 2,
             userId: 8,
             textcontent: "Mauris ullamcorper purus sit amet nulla.",
             imageUrl: "http://dummyimage.com/220x100.png/dddddd/000000",
         },
         {
-            postId: 9,
+            postId: 3,
             userId: 9,
             textcontent:
                 "Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus.",
             imageUrl: "http://dummyimage.com/194x100.png/dddddd/000000",
         },
         {
-            postId: 10,
+            postId: 3,
             userId: 10,
             textcontent:
                 "Sed accumsan felis. Ut at dolor quis odio consequat varius.",
             imageUrl: "http://dummyimage.com/101x100.png/ff4444/ffffff",
         },
         {
-            postId: 11,
+            postId: 3,
             userId: 11,
             textcontent:
                 "Praesent lectus. Vestibulum quam sapien, varius ut, blandit non, interdum in, ante.",
             imageUrl: "http://dummyimage.com/228x100.png/5fa2dd/ffffff",
         },
         {
-            postId: 12,
+            postId: 3,
             userId: 12,
             textcontent: "Phasellus sit amet erat.",
             imageUrl: "http://dummyimage.com/204x100.png/5fa2dd/ffffff",
         },
         {
-            postId: 13,
+            postId: 4,
             userId: 13,
             textcontent: "Maecenas rhoncus aliquam lacus.",
             imageUrl: "http://dummyimage.com/140x100.png/cc0000/ffffff",
         },
         {
-            postId: 14,
+            postId: 4,
             userId: 14,
             textcontent: "In hac habitasse platea dictumst.",
             imageUrl: "http://dummyimage.com/185x100.png/ff4444/ffffff",
         },
         {
-            postId: 15,
+            postId: 4,
             userId: 15,
             textcontent: "Morbi porttitor lorem id ligula.",
             imageUrl: "http://dummyimage.com/248x100.png/cc0000/ffffff",
         },
         {
-            postId: 16,
+            postId: 4,
             userId: 16,
             textcontent: "Proin risus. Praesent lectus.",
             imageUrl: "http://dummyimage.com/157x100.png/dddddd/000000",
         },
         {
-            postId: 17,
+            postId: 5,
             userId: 17,
             textcontent: "Aliquam sit amet diam in magna bibendum imperdiet.",
             imageUrl: "http://dummyimage.com/116x100.png/dddddd/000000",
         },
         {
-            postId: 18,
+            postId: 5,
             userId: 18,
             textcontent:
                 "Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.",
             imageUrl: "http://dummyimage.com/168x100.png/5fa2dd/ffffff",
         },
         {
-            postId: 19,
+            postId: 5,
             userId: 19,
             textcontent: "Aliquam erat volutpat.",
             imageUrl: "http://dummyimage.com/179x100.png/dddddd/000000",
         },
         {
-            postId: 20,
+            postId: 5,
             userId: 20,
             textcontent:
                 "Donec vitae nisi. Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.",
             imageUrl: "http://dummyimage.com/190x100.png/5fa2dd/ffffff",
         },
-    ]
+    ];
 
-    await Reply.bulkCreate(replyData)
+    await Reply.bulkCreate(replyData);
     // process.exit(0)
-}
+};
 
 // userSeed()
 
-module.exports = userSeed
+module.exports = userSeed;
