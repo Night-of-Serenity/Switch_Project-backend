791414c44fec3f63fabbc7869f4a81d22dc071e1
Peerapat Udtha(Night) <peerapat.ud@gmail.com>
2023-07-02 18:29:13 +0700

    feat(fetchAllDirectMessageContact): update

--- src/routes/chatRoute.js
+++ src/routes/chatRoute.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.get(
     "/fetchchatroom",
     authenticateMiddleware,
-    chatController.fetchChatRoom
+    chatController.fetchAllDirectMessagesContacts
 );
 
 module.exports = router;
